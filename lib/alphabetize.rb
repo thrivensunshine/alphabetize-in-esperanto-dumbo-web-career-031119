e5df6652709c7e6f087d1e3a5a18c329b48a8c43
DANIELLE ALBA <danib.sgh@gmail.com>
2019-03-03 22:27:45 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
 def alphabetize(arr)
-  # code here
+ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+    arr.sort_by do |i|
+#puts i
+    i.split("").select do |j|
+# puts j
+ALPHABET.index(j)
+
+    end
+    end
 end
